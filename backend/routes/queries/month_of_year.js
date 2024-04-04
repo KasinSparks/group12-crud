64269932a1ff857043a14792372bdc667d64a916
kasinsparks <kasinsparks@gmail.com>
2024-04-04 04:14:03 +0000

    Working commit 2

--- backend/routes/queries/month_of_year.js
+++ backend/routes/queries/month_of_year.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res, next) {
     res.send("API monthofyear test point. SUCCESS!");
 });
 
+/* http://localhost:8080/queries/monthofyear/types*/
+// Sends an array of the different types of Real Estate properties
 router.get('/types', function(req, res, next) {
     var querystr = 
             `SELECT DISTINCT Type 
@@ -26,12 +28,15 @@ router.get('/types', function(req, res, next) {
     });
 });
 
-/* http://localhost:8080/queries/monthofyear */
+/* http://localhost:8080/queries/monthofyear/avgsales */
+// NOTE: Do NOT use a production environment. Does not defend against SQL
+//       injection attacks. 
 router.get('/avgsales', function(req, res, next) {
-    console.log(req.query);
-
+    //console.log(req.query);
+    
+    // The dynamic query string
     var querystr = 
-            `SELECT SalesDate, Count(CISRealEstateSale.SalesID), Avg(AvgTemp), Avg(SnowDepth)
+            `SELECT SalesDate, Count(CISRealEstateSale.SalesID), AVG(SoldValue), Avg(AvgTemp), Avg(SnowDepth), Avg(Precipitation)
              FROM CISRealEstateSale
              INNER JOIN CISRealEstateSalesDate ON CISRealEstateSale.SalesID = CISRealEstateSalesDate.SalesID
              INNER JOIN CISWeatherSample ON CISWeatherSample.DateRec = CISRealEstateSalesDate.SalesDate
@@ -43,29 +48,43 @@ router.get('/avgsales', function(req, res, next) {
                FROM CISBusiness)) e1
                ON e1.SalesID = CISRealEstateSale.SalesID
              WHERE CISRealEstateSalesDate.SalesDate = CISWeatherSample.DateRec`;
-
+    
+    // If there is a start date, add it to the where clause
     if (req.query["fromdate"] !== undefined) {
         querystr += " AND SalesDate >= TO_DATE('" + req.query["fromdate"] + "', 'YYYY-MM-DD')";
     }
 
+    // If there is an end date, add it to the where clause
     if (req.query["todate"] !== undefined) {
         querystr += " AND SalesDate <= TO_DATE('" + req.query["todate"] + "', 'YYYY-MM-DD')";
     }
 
+    // If there is a property type specified, add it to the where clause
     if (req.query["type"] !== undefined) {
         querystr += " AND Type = '" + req.query["type"] + "'";
     }
 
-    querystr += 
-            `\nGROUP BY SalesDate
-             HAVING COUNT(CISRealEstateSale.SalesID) > 5 
-             ORDER BY SalesDate DESC`;
+    querystr += `\nGROUP BY SalesDate`;
+
+    // If there is a minumum or maximum value for a given column specified, add it to the having clause
+    if ((req.query["minval"] !== undefined || req.query["maxval"] !== undefined) && req.query["column"] !== undefined) {
+        querystr += " \nHAVING ";
+        querystr += (req.query["minval"] !== undefined ? req.query["column"] + " >= " + req.query["minval"] + " " : "");
+        if (req.query["minval"] !== undefined && req.query["maxval"] !== undefined) {
+            querystr += " AND ";
+        }
+        querystr += (req.query["maxval"] !== undefined ? req.query["column"] + " <= " + req.query["maxval"] + " " : "");
+    }
+
+    querystr += `\nORDER BY SalesDate DESC`;
     
-    // Check for if the user want the query string
+    // Check for if the user wants the query string
     /* http://localhost:8080/queries/monthofyear?showquerystr=1 */
     if (req.query["showquerystr"] !== undefined && req.query["showquerystr"] === '1') {
         res.send({ "query_str" : querystr});
     } else {
+        // User does not want the query as a string, but want to run it and get
+        // the resulting tuples
         run(querystr)
             .then(rows => {
             res.send(rows);
--- frontend/src/monthOfYearQuerys.js
+++ frontend/src/monthOfYearQuerys.js
@@ -1,7 +1,7 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip, Legend, TimeScale } from 'chart.js';
 import { Scatter } from 'react-chartjs-2';
-import moment from 'moment';
+//import moment from 'moment';
 import 'chartjs-adapter-moment';
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend, TimeScale);
 
@@ -15,40 +15,42 @@ export default function TimeOfYearQuery() {
     const [sqlstr, setSqlstr] = useState(base_url_str);
     const [showsqlcommand, setShowsqlcommand] = useState("");
     const [types, setTypes] = useState([]);
+    const [colSelect, setColSelect] = useState("COUNT(CISREALESTATESALE.SALESID)");
 
+    // Used to dynmically update the screen when the React state sqlstr is changed
     useEffect(() => {
-        let ignore = false;
+        // Get the data from the backend
         fetch(sqlstr)
             .then(res => res.json())
             .then(json => setData({ tuples: json }))
             .catch(err => err);
-
+        
+        // Used to append the showquerystr regardless if there are already
+        // GET params or not.
         var http_join_char = (sqlstr.includes('?') ? '&' : '?');
-
+        
+        // Get the query string and save it to a React state
         fetch(sqlstr + http_join_char + "showquerystr=1")
             //.then(res => setShowsqlcommand(res.body))
             .then(res => res.json())
             .then(json => setShowsqlcommand(json["query_str"]))
             .catch(err => err);
 
-        return () => {
-            ignore = true;
-        }
+        return () => {}
     }, [sqlstr]);
 
+    // Get the different real estate types from the db
     useEffect(() => {
-        let ignore = false;
         // TODO: change back to localhost
         fetch("http://192.168.20.2:8080/queries/monthofyear/types")
             .then(res => res.json())
             .then(json => setTypes(json))
             .catch(err => err);
 
-        return () => {
-            ignore = true;
-        }
+        return () => {}
     }, []);
 
+    // Options for the chartjs graph
     const options = {
         scales: {
             y: {
@@ -60,20 +62,25 @@ export default function TimeOfYearQuery() {
         },
     };
 
+    // Data for the chartjs graph
     const chart_data = {
         datasets: [
             {
-                label: 'Connecticut Daily Number of Real Estate Sales',
+                label: colSelect,
                 //data: [{x: 0, y: 1}],
                 data: data.tuples.map(el => (
-                    {x: el.SALESDATE, y: el['COUNT(CISREALESTATESALE.SALESID)']}
+                    //{x: el.SALESDATE, y: el['COUNT(CISREALESTATESALE.SALESID)']}
+                    //{x: el.SALESDATE, y: el['AVG(SOLDVALUE)']}
+                    {x: el.SALESDATE, y: el[colSelect]}
                 )),
                 backgroundColor: 'rgba(255, 99, 132, 1)',
             },
             {
                 label: 'Connecticut Daily Average Temperature',
                 data: data.tuples.map(el => (
-                    {x: el.SALESDATE, y: el['AVG(AVGTEMP)']}
+                    {x: el.SALESDATE, y: el['AVG(PRECIPITATION)']}
+                    //{x: el.SALESDATE, y: el['AVG(AVGTEMP)']}
+                    //{x: el.SALESDATE, y: el['AVG(SNOWDEPTH)']}
                 )),
                 backgroundColor: 'rgba(9, 99, 132, 1)',
                 pointStyle: 'rect',
@@ -81,6 +88,7 @@ export default function TimeOfYearQuery() {
         ],
     };
 
+    // Set the GET query (i.e. url?type=apartment&minval=2)
     function setQueryString() {
         //console.log(document.getElementById("fromdate").value);
         var url_str = base_url_str + "?";
@@ -89,9 +97,14 @@ export default function TimeOfYearQuery() {
             "fromdate" : document.getElementById("fromdate").value,
             "todate"   : document.getElementById("todate").value,
             "type"     : document.getElementById("property_type").value,
+            "column"   : document.getElementById("column_select").value,
+            "minval"   : document.getElementById("min_val").value,
+            "maxval"   : document.getElementById("max_val").value,
         };
-
+        
+        // A flag to only include a question mark at the beginning
         var is_first = true;
+        // Add all the params that have a value
         for (var k in query_params) {
             var temp_str = "";
             
@@ -100,24 +113,36 @@ export default function TimeOfYearQuery() {
                     temp_str += "&";
                 }
                 is_first = false;
-
+                
+                // Add name of param and the value
                 temp_str += k + "=" + query_params[k];
-
+                
                 url_str += temp_str;
             }
 
         }
-
+        
+        // Update the React state
         setSqlstr(url_str);
     }
-
+    
+    // Helper function to update the column for the chart data based on
+    // what the user chose
+    function setColumn() {
+        setColSelect(document.getElementById("column_select").value);
+    }
+    
+    // Add the types to a js array so we can loop over them in a map later
     var property_type_rows = [];
     for (var i = 0; i < types.length; ++i) {
         property_type_rows.push(types[i]["TYPE"]);
     }
-
+    
+    // Split the sql string into seperate lines
     var sql_command_lines = showsqlcommand.split("\n");
+    
 
+    // render
     return (
      <>
 
@@ -126,6 +151,10 @@ export default function TimeOfYearQuery() {
         <input id="fromdate" type="date" name="fromdate" />
         <label for="todate">To: </label>
         <input id="todate" type="date" name="todate" />
+        <label for="min_val">Min Value: </label>
+        <input id="min_val" type="number" name="min_val" placeholder="0" />
+        <label for="max_val">Max Value: </label>
+        <input id="max_val" type="number" name="max_val" />
         <label for="property_type">Property Type: </label>
         <select name="property_type" id="property_type">
             <option value=""></option>
@@ -133,9 +162,15 @@ export default function TimeOfYearQuery() {
                 <option value={p_type}>{p_type}</option>
             ))}
         </select>
+
+        <label for="column_select">Column: </label>
+        <select name="column_select" id="column_select" onChange={setColumn}>
+            <option value="COUNT(CISREALESTATESALE.SALESID)">COUNT(CISREALESTATESALE.SALESID)</option>
+            <option value="AVG(SOLDVALUE)">AVG(SOLDVALUE)</option>
+        </select>
         <button onClick={setQueryString}>Filter</button>
 
-        <div>
+        <div align="left">
             {sql_command_lines.map(cl => (
                 <p>{cl}</p>
             ))}
@@ -144,15 +179,3 @@ export default function TimeOfYearQuery() {
         </>
     );
 }
-
-/*
-       <div>
-         <ul>
-           {data.tuples.map(el => (
-             <li>
-               {el.SALESDATE}, {el['COUNT(*)']}, {el['AVG(AVGTEMP)']}, {el['AVG(SNOWDEPTH)']}
-             </li>
-           ))}
-         </ul>
-       </div> 
-       */
