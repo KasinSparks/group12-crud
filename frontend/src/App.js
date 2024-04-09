5e02395e88d42e09412ae37b1015be78b9465eb0
jaydenm1751 <jaydenmckenna@ufl.edu>
2024-04-09 13:52:08 -0400

    UI backbone, Login and Account creation logic done still needs database save feature.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,88 +1,82 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
+import './Account.css';
+import Login from './Login';
+import Register from './Register';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import Home from './Home'; // Assuming you have a basic Home component
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+function App() {
+    const [apiResponse, setApiResponse] = useState("");
 
-
-// https://react.dev/learn/conditional-rendering
-function TestAPIConnection({ apiRes }) {
-    if (apiRes !== "") {
-        return <p color="green" className="App-intro">{ apiRes }</p>;
-    }
-
-    return <p color="red" className="App-intro">NO CONNECTION TO BACKEND API</p>;
-}
-
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {apiResponse: "" };
-        
-        // Used as an example for Chart.js
-        this.data = {
-          labels: ['Red', 'Blue', 'Yellow'],
-          datasets: [
-            {
-              label: '# of Votes',
-              data: [42, 314, 217],
-              backgroundColor: [
-                'rgba(255,  99, 132, 0.5)',
-                'rgba( 54, 162, 235, 0.5)',
-                'rgba(255, 206,  86, 0.5)',
-              ],
-              borderColor: [
-                'rgba(255,  99, 132, 1)',
-                'rgba( 54, 162, 235, 1)',
-                'rgba(255, 206,  86, 1)',
-              ],
-              borderWidth: 1,
-            },
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [42, 314, 217],
+          backgroundColor: [
+            'rgba(255,  99, 132, 0.5)',
+            'rgba( 54, 162, 235, 0.5)',
+            'rgba(255, 206,  86, 0.5)',
           ],
+          borderColor: [
+            'rgba(255,  99, 132, 1)',
+            'rgba( 54, 162, 235, 1)',
+            'rgba(255, 206,  86, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    useEffect(() => {
+        const callAPI = () => {
+            fetch("http://localhost:8080/test")
+                .then(res => res.text())
+                .then(res => setApiResponse(res))
+                .catch(err => console.error(err));
+        };
+        callAPI();
+    }, []);
+
+    function TestAPIConnection({ apiRes }) {
+        if (apiRes !== "") {
+            return <p style={{ color: 'green' }} className="App-intro">{apiRes}</p>;
         }
-    }
-    
-    // Used as an example to test connection to the API backend
-    callAPI() {
-        fetch("http://localhost:8080/test")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
+        return <p style={{ color: 'red' }} className="App-intro">NO CONNECTION TO BACKEND API</p>;
     }
 
-    componentDidMount() {
-        this.callAPI();
-    }
-
-
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <TestAPIConnection
-              apiRes={this.state.apiResponse} 
-            />
-            <Doughnut data={this.data} />
-          </header>
-        </div>
-      );
-    }
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <div className="header-content">
+                        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                        <nav className="nav-links">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/login">Login</Link></li>
+                                {/*<li><Link to="/register">Register</Link></li>*/}
+                            </ul>
+                        </nav>
+                    </div>
+                    {/*<TestAPIConnection apiRes={apiResponse} />
+                    <Doughnut data={data} />*/}
+                </header>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main area of the app. Navigate to other pages using the links above.</p>
+    </div>
+  );
+}
+
+export default Home;
