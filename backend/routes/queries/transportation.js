7758ad48bd38a2a03349d184f6acdce420038252
AthenaWrenn <162925464+AthenaWrenn@users.noreply.github.com>
2024-04-20 16:36:01 -0400

    Update transportation.js

--- backend/routes/queries/transportation.js
+++ backend/routes/queries/transportation.js
@@ -17,10 +17,10 @@ router.get('/types', function(req, res, next) {
             `SELECT DISTINCT Type 
              FROM (
                (SELECT SalesID, Type
-               FROM CISResidential)
+               FROM KASINSPARKS.CISResidential)
                UNION 
                (SELECT SalesID, Type
-               FROM CISBusiness)
+               FROM KASINSPARKS.CISBusiness)
               )`;
 
     run(querystr)
@@ -35,7 +35,7 @@ router.get('/types', function(req, res, next) {
 router.get('/cities', function(req, res, next) {
     var querystr = 
             `SELECT DISTINCT City 
-             FROM CISLocation
+             FROM KASINSPARKS.CISLocation
              ORDER BY City DESC`;
 
     run(querystr)
